c57f470fe4f158cc5fa1c13be3d3226d694380fe
perumalraj23 <IN04530@AZENTIO.COM>
2026-07-23 17:47:38 +0530

    console for sigin issue 2

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import { createClient } from "@/lib/supabase/server";
 
 export async function GET(request: Request) {
+  console.log("VERSION: 123");
   const { searchParams, origin } = new URL(request.url);
 
   const code = searchParams.get("code");
